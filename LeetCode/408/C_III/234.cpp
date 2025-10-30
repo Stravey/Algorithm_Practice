20b2fd4d1ac3eeb8fae9e51f8633e9948931ff5f
Stravey <2205487298@qq.com>
2025-10-30 21:56:53 +0800

    update

--- LeetCode/408/C_III/234.cpp
+++ LeetCode/408/C_III/234.cpp
@@ -13,22 +13,17 @@ class Solution {
 public:
     bool isPalindrome(ListNode* head) {
         if(!head || !head -> next) return true;
-
         ListNode* fast = head;
         ListNode* slow = head;
-
         while(fast -> next && fast -> next -> next) {
             slow = slow -> next;
             fast = fast -> next -> next;
         }
-
         ListNode* firstHalf = head;
         ListNode* secondHalf = reverseList(slow -> next);
-
         ListNode* p1 = firstHalf;
         ListNode* p2 = secondHalf;
         bool flag = true;
-
         // 只需比较后半部分
         while(p2) {
             if(p1 -> val != p2 -> val) {
@@ -38,20 +33,17 @@ public:
             p1 = p1 -> next;
             p2 = p2 -> next;
         }
-
         return flag;
     }
     ListNode* reverseList(ListNode* head) {
         ListNode* prev = nullptr;
         ListNode* cur = head;
-
         while(cur) {
             ListNode* tmp = cur -> next;
             cur -> next = prev;
             prev = cur;
             cur = tmp;
         } 
-
         return prev;
     }
 };
