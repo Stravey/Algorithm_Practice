b79ccfb1a0485fa93f91c4ed4913a8a74129179e
Stravey <2205487298@qq.com>
2025-12-10 19:19:43 +0800

    update

---
+++ LeetCode/Practice/3577.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int countPermutations(vector<int>& complexity) {
+        const int mod = 1000000007;
+        long long ans = 1;
+        for(int i = 1;i < complexity.size();i++) {
+            if(complexity[i] <= complexity[0]) {
+                return 0;
+            }
+            ans = ans * i % mod;
+        }
+        return ans;
+    }
+};
+int main()
+{
+    return 0;
+}
