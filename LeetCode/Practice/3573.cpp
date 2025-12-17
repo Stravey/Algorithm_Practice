631ac1ac4ad80797ee21198a871a59ffcba748f8
Stravey <2205487298@qq.com>
2025-12-17 19:28:53 +0800

    update

---
+++ LeetCode/Practice/3573.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    long long maximumProfit(vector<int>& prices, int k) {
+        int n = prices.size();
+        vector<vector<long long>> dp(k + 1, vector<long long>(3));
+        // 初始化第 0 天的状态 
+        for (int j = 1; j <= k; j++) {
+            dp[j][1] = -prices[0];
+            dp[j][2] = prices[0];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = k; j > 0; j--) {
+                dp[j][0] = max(dp[j][0], max(dp[j][1] + prices[i], dp[j][2] - prices[i]));
+                dp[j][1] = max(dp[j][1], dp[j - 1][0] - prices[i]);
+                dp[j][2] = max(dp[j][2], dp[j - 1][0] + prices[i]);  
+            }
+        }
+        return dp[k][0];
+    }
+};
+int main()
+{
+    return 0;
+}
