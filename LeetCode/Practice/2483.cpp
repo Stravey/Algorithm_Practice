5b7e93effb326894df01b0b619f988e47ee154e5
Stravey <2205487298@qq.com>
2025-12-26 23:13:24 +0800

    update

---
+++ LeetCode/Practice/2483.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        int n = customers.size();
+        int suf = 0;
+        int pre = 0;
+        int min_cost = 0,res = 0;
+        for(int i = 0;i <= n;i++) {
+            if(min_cost > suf + pre) {
+                min_cost = suf + pre;
+                res = i;
+            }
+            if(i < n && customers[i] == 'N') {
+                pre++;
+            } else {
+                suf--;
+            }
+        }
+        return res;
+    }
+};
+int main()
+{
+    return 0;
+}
