6e9022eb6bc626deeafbc0b20dc8f919fb4960ba
Stravey <2205487298@qq.com>
2026-02-01 18:57:42 +0800

    udpate simulation

---
+++ Hero/Simulation/LC1991.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        for(int i = 0;i < nums.size();i++) {
+            int l = 0;
+            int r = 0;
+            for(int j = 0;j < i;j++) {
+                l += nums[j];
+            }
+            for(int j = i + 1;j < nums.size();j++) {
+                r += nums[j];
+            }
+            if(l == r) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
---
+++ Hero/Simulation/LCR133.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int sum = 0;
+        while(n) {
+            sum += n & 1;
+            n = (n >> 1);
+        }
+        return sum;
+    }
+};
