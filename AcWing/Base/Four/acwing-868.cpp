6a827c2beed45d6b9033a04e238e9087e44bcb85
Stravey <2205487298@qq.com>
2026-04-22 22:30:29 +0800

    check prime

---
+++ AcWing/Base/Four/acwing-868.cpp
@@ -0,0 +1,37 @@
+// 筛质数
+// O(log(log n))
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 1000010;
+
+int primes[N], cnt;
+bool st[N];
+
+void get_primes(int x)
+{
+    for(int i = 2; i <= x; i++)
+    {
+        if(!st[i])
+        {
+            primes[cnt++] = x;
+            for(int j = i + i; j <= x; j += i)
+            {
+                st[j] = true;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    get_primes(n);
+    cout << cnt << endl;
+    return 0;
+}
+
