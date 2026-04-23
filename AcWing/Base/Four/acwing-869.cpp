26c80f47af353bce41f2f3e6eec8a25eb1f741ff
Stravey <2205487298@qq.com>
2026-04-23 18:33:40 +0800

    get divisors

---
+++ AcWing/Base/Four/acwing-869.cpp
@@ -0,0 +1,49 @@
+// 求约数
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector<int> get_divisors(int x)
+{
+    vector<int> res;
+    
+    for(int i = 1; i <= x / i; i++)
+    {
+        if(x % i == 0)
+        {
+            res.push_back(i);
+            if(i != x / i) 
+            {
+                res.push_back(x / i);
+            }
+        }
+    }
+    
+    sort(res.begin(), res.end());
+    
+    return res;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    while(n --)
+    {
+        int x;
+        cin >> x;
+        auto ans = get_divisors(x);
+        for(auto t : ans) 
+        {
+            cout << t << ' ';
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
+
